7d5f92ec7c4087fceacb23ea22752038328b574c
dolapo-mosuro <mosurodolapo@gmail.com>
2025-02-06 22:35:30 +0100

    number classification api

---
+++ README.md
@@ -0,0 +1,24 @@
+# Number Classification API (Node.js + TypeScript)
+
+An API to classify numbers into various mathematical categories and retrieve fun facts.
+
+## Endpoint
+
+## Example Request
+
+```bash
+curl
+```
+
+## Example Response
+
+```
+{
+"number": 371,
+"is_prime": false,
+"is_perfect": false,
+"properties": ["armstrong", "odd"],
+"digit_sum": 11,
+"fun_fact": "371 is an Armstrong number because 3^3 + 7^3 + 1^3 = 371"
+}
+```
---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { getNumProperties, getFunFact } from "./function";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+
+app.get(
+	"/api/classify-number/:number",
+	async (req: Request, res: Response): Promise<void> => {
+		const numParams = req.params.number;
+
+		if (!numParams) {
+			res.status(400).json({ number: null, error: true });
+			return;
+		}
+
+		const num = parseInt(numParams, 10);
+		if (isNaN(num)) {
+			res.status(400).json({ number: numParams, error: true });
+			return;
+		}
+
+		const properties = getNumProperties(num);
+		try {
+			const funFact = await getFunFact(num);
+			res.status(200).json({
+				number: num,
+				is_prime: properties.isPrime,
+				is_perfect: properties.isPerfect,
+				properties: properties.properties,
+				digit_sum: properties.digitSum,
+				fun_fact: funFact,
+			});
+		} catch (error) {
+			res.status(200).json({
+				number: num,
+				is_prime: properties.isPrime,
+				is_perfect: properties.isPerfect,
+				properties: properties.properties,
+				digit_sum: properties.digitSum,
+				fun_fact: "No fun fact available.",
+			});
+		}
+	}
+);
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
