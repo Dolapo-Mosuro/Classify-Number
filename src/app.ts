316703e814e49dfc81daa97ab6f00b24b9f088a3
dolapo-mosuro <mosurodolapo@gmail.com>
2025-02-07 02:13:02 +0100

    sorting errors

--- src/app.ts
+++ src/app.ts
@@ -7,21 +7,23 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
-app.get(
-	"/api/classify-number",
-	async (req: Request, res: Response): Promise<void> => {
-		const { number } = req.query;
+app.get("/api/classify-number/:number", async (req: Request, res: Response) => {
+	const numParams = req.params.number;
 
-		if (!number || isNaN(Number(number))) {
-			res.status(400).json({ number: number || null, error: true });
-			return;
-		}
+	if (!numParams) {
+		res.status(400).json({ number: null, error: true });
+		return;
+	}
 
-		const num = parseInt(number as string, 10);
+	const num = parseInt(numParams, 10);
+	if (isNaN(num)) {
+		res.status(400).json({ number: numParams, error: true });
+		return;
+	}
 
-		const properties = getNumProperties(num);
+	const properties = getNumProperties(num);
+	try {
 		const funFact = await getFunFact(num);
-
 		res.status(200).json({
 			number: num,
 			is_prime: properties.isPrime,
@@ -30,8 +32,17 @@ app.get(
 			digit_sum: properties.digitSum,
 			fun_fact: funFact,
 		});
+	} catch (error) {
+		res.status(200).json({
+			number: num,
+			is_prime: properties.isPrime,
+			is_perfect: properties.isPerfect,
+			properties: properties.properties,
+			digit_sum: properties.digitSum,
+			fun_fact: "No fun fact available.",
+		});
 	}
-);
+});
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
