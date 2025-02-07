e334ff30b3126a3840f7c3d373feae4ea60f77f1
dolapo-mosuro <mosurodolapo@gmail.com>
2025-02-07 01:09:32 +0100

    sorting errors

--- src/app.ts
+++ src/app.ts
@@ -7,27 +7,31 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
-app.get("/api/classify-number", async (req: Request, res: Response) => {
-	const { number } = req.query;
+app.get(
+	"/api/classify-number",
+	async (req: Request, res: Response): Promise<void> => {
+		const { number } = req.query;
 
-	if (!number || isNaN(Number(number))) {
-		return res.status(400).json({ number: number || null, error: true });
+		if (!number || isNaN(Number(number))) {
+			res.status(400).json({ number: number || null, error: true });
+			return;
+		}
+
+		const num = parseInt(number as string, 10);
+
+		const properties = getNumProperties(num);
+		const funFact = await getFunFact(num);
+
+		res.status(200).json({
+			number: num,
+			is_prime: properties.isPrime,
+			is_perfect: properties.isPerfect,
+			properties: properties.properties,
+			digit_sum: properties.digitSum,
+			fun_fact: funFact,
+		});
 	}
-
-	const num = parseInt(number as string, 10);
-
-	const properties = getNumProperties(num);
-	const funFact = await getFunFact(num);
-
-	res.status(200).json({
-		number: num,
-		is_prime: properties.isPrime,
-		is_perfect: properties.isPerfect,
-		properties: properties.properties,
-		digit_sum: properties.digitSum,
-		fun_fact: funFact,
-	});
-});
+);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
